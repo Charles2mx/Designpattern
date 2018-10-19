4af89d24bd850f20ea4ec422cd091251417995e8
qiurunze <qiurunze@youxin.com>
2018-10-19 10:51:42 +0800

    test conflict!

--- src/main/java/com/geekq/design/TestSmartGit.java
+++ src/main/java/com/geekq/design/TestSmartGit.java
@@ -7,6 +7,8 @@ public class TestSmartGit {
         System.out.println("沙漠骆驼！");
         System.out.println("看我封魔！！");
         System.out.println("中国男足！！");
+        System.out.println("中国男足不服！！");
+        System.out.println("中国男足加油！！");
         System.out.println("如果感到快乐你就拍拍手！！");
         System.out.println("如果感到幸福你就拍拍手！！");
         System.out.println("如果感到幸福你就拍拍手！！");
